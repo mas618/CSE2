4a49c07995b312b8630d7951bb2bcb819b41ff42
mas618 <mas618@lehigh.edu>
2015-08-30 20:25:39 +0000

    uploading assignment

---
+++ hw01/HelloClass.java
@@ -0,0 +1,32 @@
+/////////////////////////////////////////////////////////////
+//Mark Shterk
+//HW 01
+//Hello Class Java Program
+//
+//  first compile the program
+//      javac compile the program
+//  run the program
+//      java HelloClass//
+
+//define a class
+public class HelloClass{
+    
+//  add main method
+    public static void main(String[] args) {
+//  Create various texts as defined in the assignement
+        System.out.println("  -----------"); 
+        System.out.println("  | Welcome |");
+        System.out.println("  -----------");
+        System.out.println("  ^  ^  ^  ^  ^  ^");
+        System.out.println(" / \\/ \\/ \\/ \\/ \\/ \\");
+        System.out.println("<-M--A--S--6--1--8->");
+        System.out.println(" \\ /\\ /\\ /\\ /\\ /\\ /");
+        System.out.println("  v  v  v  v  v  v");
+        System.out.println("I enjoy long walks on the beach and reading books");
+        System.out.println("by candlelight.");
+        //"\" is viewed as an escape character, having difficulty trying to fix this problem
+        
+    }
+}
+        
+        
